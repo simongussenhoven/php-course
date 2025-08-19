186ddf0110030c1e0a75206e7a1bf88c74984303
Simon Gussenhoven <simongussenhoven@gmail.com>
2025-08-19 12:39:54 +0200

    Setup template

--- index.php
+++ index.php
@@ -1,2 +1,21 @@
 <?php
-echo "Hello, World!";
+$variable = "Hello, PHP!";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Bootstrap Page</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container">
+        <h1 class="mt-5"><?php echo $variable; ?></h1>
+        <p class="lead">I guess we're really doing this</p>
+    </div>
+</body>
+
+</html>
